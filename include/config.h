f536c7812bef5cfc44731e8b9d66bc6610b0f825
Stephane <stephane.blais@gmail.com>
2026-09-16 06:19:30 -0400

    chore: bump firmware version to v1.3.1-beta for continuous backspace pre-release

--- include/config.h
+++ include/config.h
@@ -6,7 +6,7 @@
 #define DEFAULT_ROOM_NAME       "Living Room"
 #define DEFAULT_MDNS_HOSTNAME   "tv-remote"
 #define DEFAULT_AP_SSID         "TV-Remote-Setup"
-#define FIRMWARE_VERSION        "v1.3.0"
+#define FIRMWARE_VERSION        "v1.3.1-beta"
 #define GITHUB_REPO             "sblaisdev/t-dongle-s3-tv-remote"
 
 // ==========================================
