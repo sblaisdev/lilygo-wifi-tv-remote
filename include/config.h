4447a3819e340e13c773888060a612329f3473b5
Stephane <stephane.blais@gmail.com>
2026-09-17 12:30:48 -0400

    fix: switch initial web installer firmware to main branch

--- include/config.h
+++ include/config.h
@@ -6,7 +6,7 @@
 #define DEFAULT_ROOM_NAME       "Living Room"
 #define DEFAULT_MDNS_HOSTNAME   "tv-remote"
 #define DEFAULT_AP_SSID         "TV-Remote-Setup"
-#define FIRMWARE_VERSION        "v1.3.2-beta"
+#define FIRMWARE_VERSION        "v1.3.2"
 #define GITHUB_REPO             "sblaisdev/lilygo-wifi-tv-remote"
 
 // ==========================================
